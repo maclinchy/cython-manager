6071d6f72a97681d636ae3ea1dfa2e4eaf9e0f7c
Alex M <makridenko.a@yandex.ru>
2018-08-07 18:03:49 +0300

    minor changes

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_secription = fh.read()
+
 setup(name='cython-manager',
       version='0.0.1',
       author='Alexey Makridenko',
@@ -9,4 +12,5 @@ setup(name='cython-manager',
       entry_points={'console_scripts': [
             'cython-manager = CythonManager.core.managment:execute_from_command_line'
       ]},
+      install_requires=['cython'],
       packages=find_packages())
